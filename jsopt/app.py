4e33ecf32ef3a5366dfdd10503e3b80170042502
Lochie Ferrier <lochie@mit.edu>
2016-10-09 12:42:20 -0400

    cleanup

--- jsopt/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.bower import Bower
-app = Flask(__name__)
-@app.route("/")
-def main():
-	return "wrong thing"
-if __name__ == "__main__":
-	app.run()
-Bower(app)
--- jsopt/app/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask
-app = Flask(__name__,static_folder='../../static')
-app.config.from_object('config')
-from app import views
